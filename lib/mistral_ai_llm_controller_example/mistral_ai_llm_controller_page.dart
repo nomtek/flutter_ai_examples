302374768c5c864b1b770f6c0fd2e6130d0d8788
Marcin Gruchala <margruchala@gmail.com>
2024-01-26 16:01:54 +0100

    Setup UI for LLM controller

--- lib/mistral_ai_llm_controller_example/mistral_ai_llm_controller_page.dart
+++ lib/mistral_ai_llm_controller_example/mistral_ai_llm_controller_page.dart
@@ -10,15 +10,61 @@ class MistralAiLlmControllerPage extends StatefulWidget {
 
 class _MistralAiLlmControllerPageState
     extends State<MistralAiLlmControllerPage> {
+  final TextEditingController commandInputController = TextEditingController();
+  int sound = 50;
+  int temperature = 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('MistralAI LLM Controller'),
       ),
-      body: const SafeArea(
-        child: Center(
-          child: Text('MistralAI LLM Controller'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: const Text('Color'),
+                      subtitle: Container(
+                        color: Colors.red,
+                        height: 50,
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('Sound: $sound '),
+                      subtitle: Slider(
+                        min: 0,
+                        max: 100,
+                        value: sound.toDouble(),
+                        onChanged: (value) => setState(
+                          () => sound = value.toInt(),
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('Temperature (Celsius): $temperature'),
+                      subtitle: Slider(
+                        min: 15,
+                        max: 25,
+                        value: temperature.toDouble(),
+                        onChanged: (value) => setState(
+                          () => temperature = value.toInt(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              TextField(
+                controller: commandInputController,
+              ),
+            ],
+          ),
         ),
       ),
     );
