832431a185b13144bcf256917d8e2104e0ae561d
Lukasz Gawron <l.gawron@nomtek.com>
2024-01-30 14:52:41 +0100

    regenerate models.dart

--- lib/mistral_ai_book_search_example/models.g.dart
+++ lib/mistral_ai_book_search_example/models.g.dart
@@ -6,20 +6,6 @@ part of 'models.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Fragment _$FragmentFromJson(Map<String, dynamic> json) => Fragment(
-      json['text'] as String,
-      (json['embedding'] as List<dynamic>)
-          .map((e) => (e as num).toDouble())
-          .toList(),
-      (json['tokens'] as List<dynamic>).map((e) => e as int).toList(),
-    );
-
-Map<String, dynamic> _$FragmentToJson(Fragment instance) => <String, dynamic>{
-      'text': instance.text,
-      'embedding': instance.embedding,
-      'tokens': instance.tokens,
-    };
-
 SearchData _$SearchDataFromJson(Map<String, dynamic> json) => SearchData(
       (json['fragments'] as List<dynamic>).map((e) => e as String).toList(),
       (json['fragmentTokens'] as List<dynamic>)
