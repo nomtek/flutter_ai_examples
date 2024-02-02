f82c1380b10b31b239270c087f10954e1a210851
Lukasz Gawron <l.gawron@nomtek.com>
2024-02-02 10:55:31 +0100

    Setup font and theme for the example app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mistral_ai_chat_example_app/app/app.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    // register the license for the Google Fonts
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(const MyApp());
 }
