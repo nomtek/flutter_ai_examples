57364a2c9fd9ace0908e31a2f1a8539155b23647
Lukasz Gawron <l.gawron@nomtek.com>
2024-02-05 11:23:02 +0100

    Provide shared preferences, app settings and mistral client to the whole app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:mistral_ai_chat_example_app/app/app_settings/app_settings.dart';
 import 'package:mistral_ai_chat_example_app/app/router.dart';
 import 'package:mistral_ai_chat_example_app/app/theme.dart';
+import 'package:mistralai_client_dart/mistralai_client_dart.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({required this.sharedPreferences, super.key});
+
+  final SharedPreferences sharedPreferences;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-      theme: lightTheme(),
+    return MultiProvider(
+      providers: [
+        Provider<SharedPreferences>.value(value: sharedPreferences),
+        ChangeNotifierProvider<AppSettings>(
+          create: (context) =>
+              AppSettings(context.read<SharedPreferences>())..init(),
+        ),
+        ProxyProvider0(
+          update: (context, __) => MistralAIClient(
+            apiKey: context.watch<AppSettings>().mistralApiKey,
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: router,
+        theme: lightTheme(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mistral_ai_chat_example_app/app/app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   LicenseRegistry.addLicense(() async* {
     // register the license for the Google Fonts
     final license = await rootBundle.loadString('google_fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
 
-  runApp(const MyApp());
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(
+    MyApp(sharedPreferences: sharedPreferences),
+  );
 }
