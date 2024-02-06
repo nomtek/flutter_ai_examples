0bfad5a36cf782bd25796489dec9385499e0e9a8
MarcinGruchala <55763510+MarcinGruchala@users.noreply.github.com>
2024-02-06 13:49:22 +0100

    24 summary example (#30)
    * Update summary page UI and refactor code
    
    * Setup new settings UI
    
    * Refactor settings state management 
    
    ---------
    
    Co-authored-by: Łukasz Gawron <l.gawron@nomtek.com>

--- lib/mistral_ai_summary_example/mistralai_summary_page.dart
+++ lib/mistral_ai_summary_example/mistralai_summary_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:mistral_ai_chat_example_app/mistral_ai_summary_example/model.dart';
+import 'package:mistral_ai_chat_example_app/mistral_ai_summary_example/settings_model.dart';
 import 'package:mistral_ai_chat_example_app/mistral_ai_summary_example/summary_settings_page.dart';
 import 'package:mistral_ai_chat_example_app/mistral_ai_summary_example/utils.dart';
 import 'package:mistralai_client_dart/mistralai_client_dart.dart';
@@ -17,8 +17,7 @@ class _MistralAISummaryPageState extends State<MistralAISummaryPage> {
   final TextEditingController summaryInputController = TextEditingController();
 
   String summaryResult = '';
-
-  SummarySettings summarySettings = SummarySettings();
+  bool summaryInProgress = false;
 
   @override
   void dispose() {
@@ -26,24 +25,26 @@ class _MistralAISummaryPageState extends State<MistralAISummaryPage> {
     super.dispose();
   }
 
-  Future<void> summarizeText(BuildContext context, String text) async {
-    setState(() {
-      summaryResult = 'Loading...';
-    });
+  Future<void> summarizeText(String text, SummarySettings settings) async {
+    setState(() => summaryInProgress = true);
+
     try {
       final response = await context.read<MistralAIClient>().chat(
-        ChatParams(
-          model: 'mistral-medium',
-          temperature: summarySettings.temperature,
-          topP: summarySettings.topP,
-          randomSeed: summarySettings.randomSeed,
-          maxTokens: summarySettings.maxTokens,
-          safePrompt: summarySettings.safePrompt,
-          messages: [
-            ChatMessage(role: 'user', content: text),
-          ],
-        ),
-      );
+            ChatParams(
+              model: 'mistral-medium',
+              temperature: settings.temperature,
+              topP: settings.topP,
+              randomSeed: settings.randomSeed,
+              maxTokens: settings.maxTokens,
+              safePrompt: settings.safePrompt,
+              messages: [
+                ChatMessage(
+                  role: 'user',
+                  content: getSummaryPromptForText(text),
+                ),
+              ],
+            ),
+          );
       setState(() {
         summaryResult = response.choices.first.message.content;
       });
@@ -51,84 +52,160 @@ class _MistralAISummaryPageState extends State<MistralAISummaryPage> {
       setState(() {
         summaryResult = 'Error: $e';
       });
+    } finally {
+      setState(() {
+        summaryInProgress = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MistralAI Summary'),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              final newSettings = await Navigator.push(
-                context,
-                MaterialPageRoute<SummarySettings>(
-                  builder: (BuildContext context) => SettingsWidget(
-                    initialSettings: summarySettings,
-                  ),
-                ),
-              );
-              setState(() {
-                summarySettings = newSettings ?? summarySettings;
-              });
-            },
-            icon: const Icon(Icons.settings),
+    return ChangeNotifierProvider(
+      create: (_) => SummarySettingsModel(),
+      builder: (context, _) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('MistralAI Text Summary'),
+            actions: const [
+              SummarySettingsButton(),
+            ],
           ),
-        ],
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: ListView(
-            children: <Widget>[
-              TextField(
-                decoration: const InputDecoration(
-                  labelText: 'Enter text to summarize',
-                ),
-                minLines: 1,
-                maxLines: 10,
-                onChanged: (value) => summaryInputController.text = value,
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      summarizeText(context, summaryInputController.text);
-                    },
-                    child: const Text('Summarize'),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: ListView(
+                children: <Widget>[
+                  SummaryTextInput(
+                    summaryInputController: summaryInputController,
                   ),
-                  const SizedBox(width: 20),
-                  Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          summarizeText(context, summaryTextSample);
-                        },
-                        child: const Text('Summarize sample tex'),
-                      ),
-                      const SizedBox(height: 20),
-                      TextButton(
-                        onPressed: () => showDialog<String>(
-                          context: context,
-                          builder: (context) => const SampleTextDialogButton(),
-                        ),
-                        child: const Text('See sample text'),
-                      ),
-                    ],
+                  const SizedBox(height: 20),
+                  SummaryActions(
+                    onSummarize: () => summarizeText(
+                      summaryInputController.text,
+                      context.read<SummarySettingsModel>().settings,
+                    ),
+                    summaryInputController: summaryInputController,
+                  ),
+                  const SizedBox(height: 20),
+                  SummaryResultText(
+                    summaryResult: summaryResult,
+                    isLoading: summaryInProgress,
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              const Text('Summarized Text:'),
-              const SizedBox(height: 10),
-              SelectableText(summaryResult),
-            ],
+            ),
           ),
+        );
+      },
+    );
+  }
+}
+
+class SummaryActions extends StatelessWidget {
+  const SummaryActions({
+    required this.onSummarize,
+    required this.summaryInputController,
+    super.key,
+  });
+
+  final VoidCallback onSummarize;
+  final TextEditingController summaryInputController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      alignment: WrapAlignment.center,
+      runSpacing: 16,
+      children: [
+        ElevatedButton(
+          onPressed: onSummarize,
+          child: const Text('Summarize'),
+        ),
+        const SizedBox(width: 16),
+        SampleTextPasteButton(
+          summaryInputController: summaryInputController,
+        ),
+      ],
+    );
+  }
+}
+
+class SummaryResultText extends StatelessWidget {
+  const SummaryResultText({
+    required this.summaryResult,
+    required this.isLoading,
+    super.key,
+  });
+
+  final String summaryResult;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    if (summaryResult.isEmpty) {
+      return const SizedBox.shrink();
+    }
+    return Column(
+      children: [
+        Text(
+          'Summary:',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        const SizedBox(height: 10),
+        SelectableText(summaryResult),
+      ],
+    );
+  }
+}
+
+class SummaryTextInput extends StatelessWidget {
+  const SummaryTextInput({
+    required this.summaryInputController,
+    super.key,
+  });
+
+  final TextEditingController summaryInputController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autofocus: true,
+      controller: summaryInputController,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Enter text to summarize',
+      ),
+      minLines: 1,
+      maxLines: 10,
+      onChanged: (value) => summaryInputController.text = value,
+    );
+  }
+}
+
+class SampleTextPasteButton extends StatelessWidget {
+  const SampleTextPasteButton({
+    required this.summaryInputController,
+    super.key,
+  });
+
+  final TextEditingController summaryInputController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        summaryInputController.text = summaryTextSample;
+      },
+      child: Text(
+        'Paste sample text',
+        style: TextStyle(
+          decoration: TextDecoration.underline,
+          decorationStyle: TextDecorationStyle.solid,
+          decorationColor: Theme.of(context).primaryColor,
         ),
       ),
     );
@@ -172,6 +249,29 @@ class SampleTextDialogButton extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class SummarySettingsButton extends StatelessWidget {
+  const SummarySettingsButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final settingsModel = context.watch<SummarySettingsModel>();
+    return IconButton(
+      onPressed: () => Navigator.push(
+        context,
+        MaterialPageRoute<SummarySettings>(
+          builder: (_) => ChangeNotifierProvider.value(
+            value: settingsModel,
+            child: const SettingsWidget(),
+          ),
+        ),
+      ),
+      icon: const Icon(Icons.settings),
     );
   }
 }
--- lib/mistral_ai_summary_example/summary_settings_page.dart
+++ lib/mistral_ai_summary_example/summary_settings_page.dart
@@ -1,182 +1,259 @@
 import 'package:flutter/material.dart';
-import 'package:mistral_ai_chat_example_app/mistral_ai_summary_example/model.dart';
+import 'package:mistral_ai_chat_example_app/app/theme.dart';
 
-class SettingsWidget extends StatefulWidget {
+import 'package:mistral_ai_chat_example_app/mistral_ai_summary_example/settings_dialogs.dart';
+import 'package:mistral_ai_chat_example_app/mistral_ai_summary_example/settings_model.dart';
+import 'package:provider/provider.dart';
+
+class SettingsWidget extends StatelessWidget {
   const SettingsWidget({
-    required this.initialSettings,
     super.key,
   });
 
-  final SummarySettings initialSettings;
-
-  @override
-  State<SettingsWidget> createState() => _SettingsWidgetState();
-}
-
-class _SettingsWidgetState extends State<SettingsWidget> {
-  SummarySettings summarySettings = SummarySettings();
-
-  final TextEditingController modelController = TextEditingController();
-  final TextEditingController maxTokensController = TextEditingController();
-  final TextEditingController randomSeedController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    summarySettings
-      ..maxTokens = widget.initialSettings.maxTokens
-      ..model = widget.initialSettings.model
-      ..randomSeed = widget.initialSettings.randomSeed
-      ..safePrompt = widget.initialSettings.safePrompt
-      ..temperature = widget.initialSettings.temperature
-      ..topP = widget.initialSettings.topP;
-  }
-
-  @override
-  void dispose() {
-    modelController.dispose();
-    maxTokensController.dispose();
-    randomSeedController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    // to long to put in one line
-    final temperatureStringValue =
-        summarySettings.temperature.toStringAsFixed(2);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
+    return DarkerBackgroundTheme(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Settings')),
+        body: Column(
           children: [
             Expanded(
               child: ListView(
-                children: [
-                  ListTile(
-                    title: Text('Model: ${summarySettings.model.name}'),
-                    subtitle: DropdownButton<MistralAIModel>(
-                      value: summarySettings.model,
-                      onChanged: (MistralAIModel? newValue) {
-                        if (newValue == null) {
-                          return;
-                        }
-                        setState(() {
-                          summarySettings.model = newValue;
-                        });
-                      },
-                      items: MistralAIModel.values
-                          .map<DropdownMenuItem<MistralAIModel>>(
-                              (MistralAIModel model) {
-                        return DropdownMenuItem<MistralAIModel>(
-                          value: model,
-                          child: Text(model.name),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                  ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Temperature: $temperatureStringValue'),
-                        const Text(
-                          'We generally recommend altering '
-                          'this or Top P but not both.',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                    subtitle: Slider(
-                      value: summarySettings.temperature,
-                      label: summarySettings.temperature.toStringAsFixed(2),
-                      onChanged: (value) => setState(
-                        () => summarySettings.temperature =
-                            double.parse(value.toStringAsFixed(2)),
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Top P: ${summarySettings.topP.toStringAsFixed(2)}',
-                        ),
-                        const Text(
-                          'We generally recommend altering '
-                          'this or Temperature but not both.',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                    subtitle: Slider(
-                      value: summarySettings.topP,
-                      label: summarySettings.topP.toStringAsFixed(2),
-                      onChanged: (value) => setState(
-                        () => summarySettings.topP =
-                            double.parse(value.toStringAsFixed(2)),
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    title: Text('Max tokens: '
-                        '${summarySettings.maxTokens ?? 'unlimited'}'),
-                    subtitle: TextField(
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) => setState(
-                        () => summarySettings.maxTokens = int.tryParse(value),
-                      ),
-                      controller: maxTokensController,
-                    ),
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Safe prompt: ${summarySettings.safePrompt}',
-                    ),
-                    trailing: Switch(
-                      value: summarySettings.safePrompt,
-                      onChanged: (value) =>
-                          setState(() => summarySettings.safePrompt = value),
-                    ),
-                  ),
-                  ListTile(
-                    title: Text('Random seed: '
-                        '${summarySettings.randomSeed ?? 'none'}'),
-                    subtitle: TextField(
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) => setState(
-                        () => summarySettings.randomSeed = int.tryParse(value),
-                      ),
-                      controller: randomSeedController,
-                    ),
-                  ),
+                children: const [
+                  ModelSettingWidget(),
+                  SettingsListSpacer(),
+                  TemperatureAndTopPSettingWidget(),
+                  SettingsListSpacer(),
+                  MaxTokensSettingWidget(),
+                  SettingsListSpacer(),
+                  RandomSeedSettingWidget(),
+                  SettingsListSpacer(),
+                  SafePromptSettingWidget(),
                 ],
               ),
             ),
-            ButtonBar(
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ModelSettingWidget extends StatefulWidget {
+  const ModelSettingWidget({
+    super.key,
+  });
+
+  @override
+  State<ModelSettingWidget> createState() => _ModelSettingWidgetState();
+}
+
+class _ModelSettingWidgetState extends State<ModelSettingWidget> {
+  MistralAIModel selectedModel = MistralAIModel.mistralMedium;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClickableSettingsItem(
+      settingName: 'Model',
+      settingValue: context.watch<SummarySettingsModel>().settings.model.name,
+      onTap: () {
+        final summarySettingsModel = context.read<SummarySettingsModel>();
+        showDialog<void>(
+          context: context,
+          builder: (_) => ChangeNotifierProvider.value(
+            value: summarySettingsModel,
+            child: const ModelSettingsDialog(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class TemperatureAndTopPSettingWidget extends StatelessWidget {
+  const TemperatureAndTopPSettingWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final temperature =
+        context.watch<SummarySettingsModel>().settings.temperature;
+
+    // to long to put in one line
+    final temperatureStringValue = temperature.toStringAsFixed(2);
+
+    final topP = context.watch<SummarySettingsModel>().settings.topP;
+
+    return ColoredBox(
+      color: Theme.of(context).colorScheme.surface,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 28,
+          vertical: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context, summarySettings);
-                  },
-                  child: const Text('Save'),
+                Text(
+                  'Temperature: $temperatureStringValue',
+                ),
+                Slider(
+                  value: temperature,
+                  label: temperature.toStringAsFixed(2),
+                  onChanged: (value) => context
+                      .read<SummarySettingsModel>()
+                      .setTemperature(value),
                 ),
               ],
             ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Top P: ${topP.toStringAsFixed(2)}',
+                ),
+                Slider(
+                  value: topP,
+                  label: topP.toStringAsFixed(2),
+                  onChanged: (value) =>
+                      context.read<SummarySettingsModel>().setTopP(value),
+                ),
+              ],
+            ),
+            const Text(
+              'We recommend altering only one, not both',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MaxTokensSettingWidget extends StatelessWidget {
+  const MaxTokensSettingWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClickableSettingsItem(
+      settingName: 'Max tokens',
+      settingValue: context
+              .watch<SummarySettingsModel>()
+              .settings
+              .maxTokens
+              ?.toString() ??
+          'Unlimited',
+      onTap: () {
+        final summarySettingsModel = context.read<SummarySettingsModel>();
+        showDialog<void>(
+          context: context,
+          builder: (_) => ChangeNotifierProvider.value(
+            value: summarySettingsModel,
+            child: const MaxTokensDialog(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class RandomSeedSettingWidget extends StatelessWidget {
+  const RandomSeedSettingWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClickableSettingsItem(
+      settingName: 'Random seed',
+      settingValue: context
+              .watch<SummarySettingsModel>()
+              .settings
+              .randomSeed
+              ?.toString() ??
+          'Unset',
+      onTap: () {
+        final summarySettingsModel = context.read<SummarySettingsModel>();
+        showDialog<void>(
+          context: context,
+          builder: (_) => ChangeNotifierProvider.value(
+            value: summarySettingsModel,
+            child: const RandomSeedDialog(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class SafePromptSettingWidget extends StatelessWidget {
+  const SafePromptSettingWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final settingValue =
+        context.watch<SummarySettingsModel>().settings.safePrompt;
+    return SwitchListTile(
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 29,
+        vertical: 10,
+      ),
+      title: Text('Safe prompt: $settingValue '),
+      value: settingValue,
+      onChanged: (value) {
+        context.read<SummarySettingsModel>().setSafePrompt(safePrompt: value);
+      },
+    );
+  }
+}
+
+class SettingsListSpacer extends StatelessWidget {
+  const SettingsListSpacer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 24);
+  }
+}
+
+class ClickableSettingsItem extends StatelessWidget {
+  const ClickableSettingsItem({
+    required this.settingName,
+    required this.settingValue,
+    required this.onTap,
+    super.key,
+  });
+
+  final String settingName;
+  final String settingValue;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Theme.of(context).colorScheme.surface,
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 29,
+          vertical: 8,
+        ),
+        title: Text(settingName),
+        subtitle: Text(settingValue),
+        onTap: onTap,
       ),
     );
   }
